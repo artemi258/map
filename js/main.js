49aae408de2e6490d81d0ebab95048d7381c44b7
Artem Grebnev <70155735+artemi258@users.noreply.github.com>
2023-09-05 20:20:30 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const paths = document.querySelectorAll("path");
 const map = document.querySelector("#map");
+const paths = map.querySelectorAll("path");
 const blockInfo = document.querySelectorAll(".info");
 
-map.addEventListener(
-  "click",
-  (e) => {
-    e.stopPropagation();
-    const target = e.target;
-    const region = target.getAttribute("data-region");
+map.addEventListener("click", (e) => {
+  const target = e.target;
+  const region = target.getAttribute("data-region");
+  if (region) {
     blockInfo.forEach((elem) => {
       elem.style.display = "none";
     });
     paths.forEach((elem) => {
-      elem.style.fill = "black";
+      elem.style.fill = "aqua"; // цвет как в css стилях '.mapSVG path'
+      elem.style.opacity = "0";
     });
     for (let i = 0; i < blockInfo.length; i++) {
       const blockRegion = blockInfo[i].getAttribute("data-region");
       if (region === blockRegion) {
         blockInfo[i].style.display = "block";
-        target.style.fill = "red";
+        target.style.fill = "red"; // цвет при клике
+        target.style.opacity = "1";
+        break;
       }
     }
-  },
-  true
-);
+  }
+});
